a21f79d4441edc1f577b5f3d319b9203836ef663
ropey <nicolasborromei@gmail.com>
2020-10-19 18:02:41 -0300

    TINCHARDO SEGUIS VOS CRAC

--- mapeo.c
+++ mapeo.c
@@ -5,23 +5,29 @@
 #define MAP_ERROR_MEMORIA           100
 
 void crear_mapeo(tMapeo * m, int ci, int (*fHash)(void *), int (*fComparacion)(void *, void *)) {
-    (*m) = (tMapeo)malloc(sizeof(struct entrada));
+    (*m) = (tMapeo) malloc(sizeof(struct entrada));
 
     if(*m == NULL) {
         exit(MAP_ERROR_MEMORIA);
     }
 
-    (*m)->tabla_hash = (tLista) malloc(sizeof(struct celda));
+    crear_lista((*m)->tabla_hash);
 
     if(ci < 10) {
         (*m)->longitud_tabla = 10;
     } else {
         (*m)->longitud_tabla = ci;
     }
-
     (*m)->cantidad_elementos = 0;
-
     (*m)->hash_code = fHash;
-
     (*m)->comparador = fComparacion;
+}
+
+tValor m_insertar(tMapeo m, tClave c, tValor v) {
+    if (m->cantidad_elementos/ m->longitud_tabla > 0.75 * m->longitud_tabla) {
+        printf("is rompido (>factor de carga)\n");
+    } else {
+        l_insertar(*m->tabla_hash, l_primera(*m->tabla_hash), v);
+        m->cantidad_elementos = m->cantidad_elementos + 1;
+    }
 }
