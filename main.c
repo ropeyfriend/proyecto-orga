8a0fabd2176627eddd7baf22fcfc02007b7dac22
ropey <nicolasborromei@gmail.com>
2020-10-20 17:32:47 -0300

    asco

--- main.c
+++ main.c
@@ -5,34 +5,53 @@
 
 int main() {
     /*
-    tLista lista;           //variable
-    crear_lista(&lista);    //variable --> celda[elemento: null, siguiente: null]
+    tLista listarda;
+    crear_lista(&listarda);
 
-    tElemento p1 = "tincho";
-    tElemento p2 = "cache";
-    tElemento p3 = "niko";
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+
+    l_insertar(listarda, listarda, &a);
+    l_insertar(listarda, listarda->siguiente, &b);
+    l_insertar(listarda, listarda->siguiente->siguiente, &c);
+    l_insertar(listarda, listarda->siguiente->siguiente->siguiente, &d);
+
+    int * r1 = listarda->siguiente->elemento;
+    int * r2 = listarda->siguiente->siguiente->elemento;
+    int * r3 = listarda->siguiente->siguiente->siguiente->elemento;
+    int * r4 = listarda->siguiente->siguiente->siguiente->siguiente->elemento;
+
+    printf("%i \n", *r1);
+    printf("%i \n", *r2);
+    printf("%i \n", *r3);
+    printf("%i \n", *r4);
+
+    int * primera = l_primera(listarda)->siguiente->elemento;
 
 
-    l_insertar(lista, l_primera(lista), p3);
-    l_insertar(lista, l_primera(lista), p2);
-    l_insertar(lista, l_primera(lista), p1);
-
-    printf("0: %s \n", lista->elemento);
-    printf("1: %s \n", l_primera(lista)->siguiente->elemento);
-    printf("2: %s \n", l_primera(lista)->siguiente->siguiente->elemento);
-    printf("3: %s \n", l_primera(lista)->siguiente->siguiente->siguiente->elemento);
+    printf("\n");
+    printf("%i \n", *primera);
     */
 
-    tMapeo mapardox;
-    crear_mapeo(&mapardox,10,NULL,NULL);
-    printf("%i \n",mapardox->cantidad_elementos);
-    tClave c = "tincho";
-    tValor v = "tincharda";
-    m_insertar(mapardox,c,v);
+    tMapeo map;
+    crear_mapeo(&map, 10, NULL, NULL);
 
-    printf("%i \n",mapardox->cantidad_elementos);
-    printf("%s \n",l_primera(mapardox->tabla_hash)->elemento);
+    printf("cant elementos: %i \n", map->cantidad_elementos);
 
+    int c = 1;
+    int v = 10;
+
+    tClave clave;
+    clave = &c;
+
+    tValor valor = malloc(sizeof(tValor));
+    valor = &v;
+
+    m_insertar(map, clave, valor);
+
+    tEntrada * recuperado = (*(map->tabla_hash))->elemento;
 
     return 0;
 }
--- mapeo.c
+++ mapeo.c
@@ -4,36 +4,83 @@
 
 #define MAP_ERROR_MEMORIA           100
 
-void crear_mapeo(tMapeo * m, int ci, int (*fHash)(void *), int (*fComparacion)(void *, void *)) {
+/* EXPLICACION FEDE:
+ * Este campo del mapeo define un ARREGLO de listas. Noten que aquí NO se reserva memoria para
+ * cada componente del arreglo, esto se deberá hacer cuando se crea el mapeo.
+ * tLista * tabla_hash;
+ * Esta definición permite manipular mi_arreglo_enteros y mi_arreglo_enteros_2 de forma similar
+ * Tener en cuenta que la reservación de memoria NO es equivalente.
+ * int [10] mi_arreglo_enteros;
+ * int * mi_arreglo_enteros_2 = malloc(10 * sizeof(int))
+ *
+ * Ejemplos de la equivalencia en el manejo de las variables
+ * mi_arreglo_enteros[0] = 10;
+ * mi_arreglo_enteros_2 = 10;
+ * mi_arreglo_enteros[5] = 50;
+ *
+ * *(mi_arreglo_enteros_2 +5) = 50;
+ * mi_arreglo_enteros[i] = x; es equivalente a *(mi_arreglo_enteros_2+i) = x;
+ */
+
+void crear_mapeo(tMapeo *m, int ci, int (*fHash)(void *), int (*fComparacion)(void *, void *)) {
     (*m) = (tMapeo) malloc(sizeof(struct entrada));
-
-    if(*m == NULL) {
+    if (*m == NULL)
         exit(MAP_ERROR_MEMORIA);
-    }
-
-    tLista L;
-    crear_lista(&L);
-    (*m)->tabla_hash = &L;
-
-    if(ci < 10) {
+    if (ci < 10)
         (*m)->longitud_tabla = 10;
-    } else {
+    else
         (*m)->longitud_tabla = ci;
-    }
+
     (*m)->cantidad_elementos = 0;
     (*m)->hash_code = fHash;
     (*m)->comparador = fComparacion;
+
+    (*m)->tabla_hash = malloc((*m)->longitud_tabla * sizeof(tLista));
+
+    for (int i =0; i < (*m)->longitud_tabla; i++) {
+        crear_lista((*m)->tabla_hash + i);
+    }
 }
 
+/* EXPLICACION FEDE 2:
+ * La signatura de la función recuperar de la lista es la siguiente:
+ * 1) tElemento l_recuperar(tLista l, tPosicion p);
+ * 2) void * l_recuperar(struct celda * l, struct celda * p);
+ * Desde el cliente:
+ * tLista mi_lista;
+ * tElemento mi_elemento;
+ * int mi_entero = 10;
+ *
+ * crear_lista(&mi_lista);
+ * l_insertar(mi_lista, l_primera(mi_lista), &mi_entero);
+ *
+ * mi_elemento = l_recuperar(mi_lista, l_primera(mi_lista));
+ * int * otro_entero = (int *) l_recuperar(mi_lista, l_primera(mi_lista));
+ *                    unico casteo ever!!
+*/
 tValor m_insertar(tMapeo m, tClave c, tValor v) {
-    if (m->cantidad_elementos/ m->longitud_tabla > 0.75 * m->longitud_tabla) {
+    if (m->cantidad_elementos / m->longitud_tabla > 0.75 * m->longitud_tabla) {
         printf("is rompido (>factor de carga)\n");
     } else {
-        tEntrada entrada = (tEntrada)malloc(sizeof(entrada));
+        tEntrada entrada = (tEntrada) malloc(sizeof(struct entrada));
         entrada->clave = c;
         entrada->valor = v;
 
-        l_insertar(*m->tabla_hash, l_primera(*m->tabla_hash), entrada);
+        //para acceder a una lista del "arreglo" tabla_hash habria que hacer:
+        // (*(m->tabla_hash + i)) = celda { elemento , siguiente }
+        l_insertar(*(m->tabla_hash), l_primera(*(m->tabla_hash)), entrada);
+        tEntrada * e = l_recuperar(*(m->tabla_hash), l_primera(*(m->tabla_hash)));
+
+        int * r1 = (*e)->clave;
+        printf("gas");
+
+        printf("%i \n", *r1);
+
         m->cantidad_elementos = m->cantidad_elementos + 1;
     }
+    return 0;
+}
+
+tValor m_recuperar(tMapeo m, tClave c) {
+
 }
